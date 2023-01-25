336ddcd4fd9f5a2ac4fdeab30d8670b681b90aae
makyia bonner <makyiabonner@gmail.com>
2023-01-25 15:24:27 -0500

    Form Layout Completed

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
+
+
 document.querySelector(".newBook-hover").addEventListener('click', () =>
 document.querySelector('.screen').classList.toggle('blurry')
 )
 
-let book = (bookName,pagesRead,completed) => {
-    return {bookName, pagesRead, completed};
+let book = (bookName,pagesRead, pagesTotal, completed) => {
+    let completePerc = completion(pagesRead,pagesTotal)
+    return {bookName, pagesRead, completePerc , completed};
 }
+let r = book("rey", 40, 120, false)
+console.log(r)
 createBook
 editBook
 deleteBook
 function completion(a,b){
-    let percent = (a / b) * 100;
+    let percent = Math.floor((a / b) * 100);
     return `${percent}% DONE`
 }
