876f48a5c98537239e68c675f8a03350c596c94a
makyia bonner <makyiabonner@gmail.com>
2023-01-25 13:10:34 -0500

    Created blur effect for entire screen .onclick

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
-let addBook = (bookName,pagesRead,completed) => {
+document.querySelector(".newBook-hover").addEventListener('click', () =>
+document.querySelector('.screen').classList.toggle('blurry')
+)
+
+let book = (bookName,pagesRead,completed) => {
     return {bookName, pagesRead, completed};
 }
+createBook
+editBook
+deleteBook
