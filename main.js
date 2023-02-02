d3d3a59e38a47752e3cb73f606304d34688b3f6f
makyia bonner <makyiabonner@gmail.com>
2023-02-01 21:50:05 -0500

    Create book now fully functional in the DOM

--- main.js
+++ main.js
@@ -5,7 +5,31 @@ let submit = document.querySelector('#submit');
 let bookN = document.querySelector("#bookName");
 let pagesR  = document.querySelector("#currPage");
 let pagesT = document.querySelector("#bookPage");
+let main = document.querySelector(".main");
 let complete = document.querySelector('input[name = "yes-no"]:checked');
+const prevBook = document.createElement('div');
+const bookTitle = document.createElement('div');
+const titleText = document.createElement('p')
+const bookHover = document.createElement('div');
+const comp = document.createElement('p');
+const perc = document.createElement('p');
+prevBook.className = 'prevBook';
+bookTitle.className = 'bookTitle';
+titleText.className = 'bookTitle-text';
+bookHover.className = 'prevBook-hover';
+comp.className = 'completion';
+perc.className = 'percent';
+prevBook.appendChild(bookTitle);
+bookTitle.appendChild(titleText);
+prevBook.appendChild(bookHover);
+bookHover.appendChild(comp);
+bookHover.appendChild(perc);
+//<div class="newBook-hover">
+//            <div class="newBook">
+//                <p>+</p>
+//            </div>
+//            <p>Add New Book</p>
+//        </div>
 let permDelete = document.querySelector('.permDelete');
 let edit = document.querySelector('#edit');
 let del = document.querySelector('#delete');
@@ -39,16 +63,24 @@ submit.addEventListener('click', () => {
     createBook();
 })
 
-let book = (bookName,pagesRead, pagesTotal, completed) => {
-    let completePerc = completion(pagesRead,pagesTotal)
-    return {bookName, pagesRead, completePerc , completed};
+let book = (bookName, pagesRead, pagesTotal, completed) => {
+    let completePerc = completion(pagesRead,pagesTotal);
+    return {bookName, pagesRead, pagesTotal , completed, completePerc};
 }
 
+//,complete.value
 function createBook(){
-    library.push(book(bookN.value,pagesR.value,pagesT.value,complete.value));
-    //for loop to iterate thru library array
+    library.push(book(bookN.value,pagesR.value,pagesT.value));
+    for(i = 0; i <= library.length - 1; i++){
+        main.appendChild(prevBook);
+        titleText.textContent = library[i].bookName;
+        comp.textContent = 'completion';
+        perc.textContent = completion(library[i].pagesRead, library[i].pagesTotal);
+        
+        //        <input type="button" id="delete" name="Delete" value="Delete" />
+        //        <input type="button" id="edit" name="Edit" value="Edit" />
+    }
     //change the DOM content for library
-    console.log(library);
 }
 function editBook(){
     //when click opens selected library index
@@ -62,5 +94,5 @@ function deleteBook(){
 }
 function completion(a,b){
     let percent = Math.floor((a / b) * 100);
-    return `${percent}% DONE`;
+    return `${percent}%`;
 }
